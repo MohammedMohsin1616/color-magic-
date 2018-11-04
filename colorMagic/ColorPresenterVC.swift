e23d4878b13b9200aa9ca4466148a755c870bf95
Mohammed <mohammedmohsin68217@gmail.com>
2018-11-04 23:02:14 +0200

    Refactoring

--- colorMagic/ColorPresenterVC.swift
+++ colorMagic/ColorPresenterVC.swift
@@ -24,7 +24,9 @@ class ColorPresenterVC: UIViewController, ColorTransferDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
